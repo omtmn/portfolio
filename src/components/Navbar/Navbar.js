f8c82e13cfefa5c5ff095525982da7c61b0e2532
Alex <omtmn@icloud.com>
2021-11-25 04:53:58 -0500

    Navigation + Sidebar(wip)

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { 
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileMenu, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavEl'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/"> dolla </NavLogo>
+                    <MobileMenu>
+                        <FaBars/>
+                    </MobileMenu>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="home">
+                                Home 
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="about">
+                                About 
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="projects">
+                                Projects 
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="contact">
+                                Contact 
+                            </NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="/login">Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
