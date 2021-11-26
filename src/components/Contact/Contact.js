abead5a123960d656d2e57a7b63a8a755575fd00
Alex <omtmn@icloud.com>
2021-11-26 06:24:46 -0500

    a little more confetti wouldnt hurt?

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import svg1  from '../../assets/svg-1.svg'
+import Reward from "react-rewards";
+import svg1 from "../../assets/svg-1.svg";
 import Fade from "react-reveal/Fade";
-import res from '../../assets/resume.pdf'
+import res from "../../assets/resume.pdf";
 import {
   AboutContainer,
   AboutWrapper,
@@ -17,66 +18,89 @@ import {
   ArrowRight,
   List,
   Item,
-  Anchor
+  Anchor,
 } from "./ContactEl";
 import { Button } from "../ButtonEl";
 import { Heading } from "../About/AboutEl";
 
 const Contact = () => {
-    const [hover, setHover] = useState(false);
+  const [hover, setHover] = useState(false);
+  let reward;
+  const handleFun = () => {
+    reward.rewardMe();
+  };
+  const onHover = () => {
+    setHover(!hover);
+  };
 
-    const onHover = () => {
-      setHover(!hover);
-    };
-  
-    return (
-        <>
-        <AboutContainer id='contact'>
-          <AboutWrapper>
-            <AboutRow>
-              <Column1>
-                <TextWrapper>
-                  <Fade left> <Heading>Contact</Heading> </Fade>
-                  <Fade top>
-                    <Subtitle>
-                        <List>
-                            <Item href="https://www.linkedin.com/in/omtmn/">LinkedIn</Item>
-                        </List>
-                        <List>
-                            <Item href="https://github.com/omtmn">Github</Item>
-                        </List>
-                        <List>
-                            <Item href="mailto:alexscodespace@gmail.com">Email</Item>
-                        </List>
-                        <List>
-                            <Item href=""><Anchor href={res} download="Alex Edwards.pdf">Download Resume</Anchor></Item>
-                        </List>
-                    </Subtitle>
-                  </Fade>
-                  <BtnWrap>
+  return (
+    <>
+      <AboutContainer id="contact">
+        <AboutWrapper>
+          <AboutRow>
+            <Column1>
+              <TextWrapper>
+                <Fade left>
+                  {" "}
+                  <Heading>Contact</Heading>{" "}
+                </Fade>
+                <Fade top>
+                  <Subtitle>
+                    <List>
+                      <Item href="https://www.linkedin.com/in/omtmn/">
+                        LinkedIn
+                      </Item>
+                    </List>
+                    <List>
+                      <Item href="https://github.com/omtmn">Github</Item>
+                    </List>
+                    <List>
+                      <Item href="mailto:alexscodespace@gmail.com">Email</Item>
+                    </List>
+                    <List>
+                      <Item href="">
+                        <Anchor href={res} download="Alex Edwards.pdf">
+                          Download Resume
+                        </Anchor>
+                      </Item>
+                    </List>
+                  </Subtitle>
+                </Fade>
+                <BtnWrap>
+                  <Reward
+                    ref={(ref) => {
+                      reward = ref;
+                    }}
+                    type="confetti"
+                  >
                     <Button
-                      smooth={true} duration={700} spy={true} exact='true'
+                      smooth={true}
+                      duration={2500}
+                      spy={true}
+                      exact="true"
+                      onClick={handleFun}
+                      to="home"
                       primary="true"
                       dark="true"
-                      to="home"
                       onMouseEnter={onHover}
                       onMouseLeave={onHover}
                     >
                       Back to home {hover ? <ArrowForward /> : <ArrowRight />}
                     </Button>
-                  </BtnWrap>
-                </TextWrapper>
-              </Column1>
-              <Column2>
-                <ImgWrap>
-                  <Img src={svg1}></Img>
-                </ImgWrap>
-              </Column2>
-            </AboutRow>
-          </AboutWrapper>
-        </AboutContainer>
-      </>
-    )
-}
+                  </Reward>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={svg1}></Img>
+              </ImgWrap>
+            </Column2>
+          </AboutRow>
+        </AboutWrapper>
+      </AboutContainer>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Video from "../../videos/video.mp4";
+import Reward from "react-rewards";
 import Fade from "react-reveal/Fade";
 import {
   HeroContainer,
@@ -13,34 +14,55 @@ import {
   ArrowRight,
 } from "./HeroEl";
 import { Button } from "../ButtonEl";
+import SpecialButton from "../fun/SpecialButton";
 
 const Hero = () => {
   const [hover, setHover] = useState(false);
+
+  let reward;
 
   const onHover = () => {
     setHover(!hover);
   };
 
+  const handleFun = () => {
+    reward.rewardMe();
+  };
+
   return (
-    <HeroContainer id='home'>
+    <HeroContainer id="home">
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
       <HeroContent>
         <Fade top>
           <HeroHeader> Hello, World </HeroHeader>
-          <HeroP> Click below to contact <br/> or scroll to learn more </HeroP>
+          <HeroP>
+            {" "}
+            Click below to contact <br /> or scroll to learn more{" "}
+          </HeroP>
           <HeroBtnWrapper>
-            <Button
-            smooth={true} duration={700} spy={true} exact='true'
-              to="contact"
-              primary="true"
-              dark="true"
-              onMouseEnter={onHover}
-              onMouseLeave={onHover}
+            <Reward
+              ref={(ref) => {
+                reward = ref;
+              }}
+              type="confetti"
             >
-              Contact {hover ? <ArrowForward /> : <ArrowRight />}
-            </Button>
+              <Button
+                smooth={true}
+                duration={2500}
+                spy={true}
+                exact="true"
+                onClick={handleFun}
+                to="contact"
+                primary="true"
+                dark="true"
+                onMouseEnter={onHover}
+                onMouseLeave={onHover}
+              >
+                Contact {hover ? <ArrowForward /> : <ArrowRight />}
+              </Button>
+            </Reward>
           </HeroBtnWrapper>
         </Fade>
       </HeroContent>
